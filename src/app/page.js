c14d5eea162f6cdcb06798deeceee2fd8c90e1bc
andy201512 <1743535684@qq.com>
2023-12-16 04:03:49 +0800

    完成水印图片合成基础功能。

--- src/app/page.js
+++ src/app/page.js
@@ -7,15 +7,13 @@ export default function Watermark() {
 
   const previewBackgroundRef = useRef(null);
   const previewWatermarkRef = useRef(null);
-  const dropDistance = {x: 0,y: 0};
+  const dropDistance = { x: 0, y: 0 };
 
-  // 一个获取元素定位样式数值的方法
-  function getCPS (ref, name){ return parseFloat(window.getComputedStyle(ref, null)[name]) }; 
-  
-  // 一个获取元素长宽样式数值的方法
-  function getCLS(ref, name){ return parseFloat(window.getComputedStyle(ref, null)[name === "top" ? 'height' : 'width']) }; 
+  // 获取元素样式数值的方法
+  function getCSNum(ref, name) { return parseFloat(window.getComputedStyle(ref, null)[name]) };
 
-  function getNewDistance(ref, bgRef, name, moveValue){
+  // 获取新的水印定位
+  function getNewDistance(wtRef, bgRef, name, moveValue) {
 
     // 为了限制水印不超出原图范围，区分情形赋值，合法赋值区间应是背景线减去水印线的宽线上
     // bgstart   ----------------------------------   bgend  背景区间
@@ -23,19 +21,23 @@ export default function Watermark() {
     // lgstart   ****************************         lgend  合法区间
 
     // 另外由于居中定位给原图和水印图都加了百分比定位和百分比平移，计算时要补上对齐
-    
-    const computedValue = getCPS(ref, name) + moveValue;
-    const bgStart = getCPS(bgRef, name) - 0.5 * getCLS(bgRef, name) + 0.5 * getCLS(ref, name);
-    const bgEnd = bgStart + getCLS(bgRef, name) - getCLS(ref, name);
-    
-    if(computedValue < bgStart) return bgStart;
-    if(computedValue > bgEnd) return bgEnd;
+
+    const computedValue = getCSNum(wtRef, name) + moveValue;
+    const bgl = getCSNum(bgRef, name === "top" ? 'height' : 'width');
+    const wtl = getCSNum(wtRef, name === "top" ? 'height' : 'width');
+
+    const bgStart = getCSNum(bgRef, name) - 0.5 * (bgl - wtl);
+    const bgEnd = bgStart + bgl - wtl;
+
+    if (computedValue < bgStart) return bgStart;
+    if (computedValue > bgEnd) return bgEnd;
     return computedValue;
   };
 
 
   function handleOriginInputChange(e) {
-    let img = new Image();
+    const img = document.createElement('img');
+    img.id = 'bgImg';
     img.src = URL.createObjectURL(e.target.files[0]);
     img.onload = function () {
       previewBackgroundRef.current.append(img)
@@ -43,7 +45,8 @@ export default function Watermark() {
   };
 
   function handleWatermarkInputChange(e) {
-    let img = new Image();
+    const img = document.createElement('img');
+    img.id = 'wtImg';
     img.src = URL.createObjectURL(e.target.files[0]);
     img.onload = function () {
       previewWatermarkRef.current.append(img)
@@ -61,16 +64,32 @@ export default function Watermark() {
     dropDistance.x = e.screenX - dropDistance.x;
     dropDistance.y = e.screenY - dropDistance.y;
 
-    const ref = previewWatermarkRef.current
     const bgRef = previewBackgroundRef.current;
-    ref.style.top = getNewDistance(ref, bgRef, 'top', dropDistance.y) + 'px';
-    ref.style.left = getNewDistance(ref, bgRef, 'left', dropDistance.x) + 'px';
+    const wtRef = previewWatermarkRef.current;
+
+
+    wtRef.style.top = getNewDistance(wtRef, bgRef, 'top', dropDistance.y) + 'px';
+    wtRef.style.left = getNewDistance(wtRef, bgRef, 'left', dropDistance.x) + 'px';
   };
 
   function handleGenerateButtonClick() {
-    let canvas = document.createElement('canvas');
-    let ctx = canvas.getContext("2d");
-    // ctx.drawImage(img, 0, 0);
+    const bgRef = previewBackgroundRef.current;
+    const wtRef = previewWatermarkRef.current;
+
+    const canvas = document.createElement('canvas',
+      {
+        height: getCSNum(bgRef, 'height') + 'px',
+        width: getCSNum(bgRef, 'width') + 'px'
+      });
+    const ctx = canvas.getContext("2d");
+
+    ctx.drawImage(document.getElementById('bgImg'), 0, 0);
+
+    // TODO: 可以梳理一下看有没有更简便的偏移坐标算法
+    let offsetX = getCSNum(wtRef, 'left') - (getCSNum(bgRef, 'left') - 0.5 * (getCSNum(bgRef, 'width') - getCSNum(wtRef, 'width')));
+    let offsetY = getCSNum(wtRef, 'top') - (getCSNum(bgRef, 'top') - 0.5 * (getCSNum(bgRef, 'height') - getCSNum(wtRef, 'height')));
+
+    ctx.drawImage(document.getElementById('wtImg'), offsetX, offsetY);
 
     const el = document.createElement('a');
     el.href = canvas.toDataURL();
